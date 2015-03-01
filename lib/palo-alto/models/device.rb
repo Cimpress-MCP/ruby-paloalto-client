290ec1279846c67f57270973f50dcbe60218a972
Justin Karimi <jekhokie@gmail.com>
2015-03-01 00:12:07 -0500

    Spec and Add Device Association with VSys
    Update the Device model to include association with VirtualSystem.

--- lib/palo-alto/models/device.rb
+++ lib/palo-alto/models/device.rb
@@ -1,22 +1,24 @@
 module PaloAlto
   module Models
     class Device
-      attr_accessor :name, :ip
+      attr_accessor :name, :ip, :virtual_systems
 
       # Create and returns a new PaloAlto::Models::Device instance with the given parameters
       #
       # == Attributes
       #
-      # * +name+ - Name of the device
-      # * +ip+   - Device IP address
+      # * +name+            - Name of the device
+      # * +ip+              - Device IP address
+      # * +virtual_systems+ - Array containing Model::VirtualSystem instances
       #
       # == Example
       #
       #  PaloAlto::Models::Device.new name: 'device-1',
       #                               ip:   '1.2.3.4'
-      def initialize(name:, ip:)
-        self.name = name
-        self.ip   = ip
+      def initialize(name:, ip:, virtual_systems: [])
+        self.name            = name
+        self.ip              = ip
+        self.virtual_systems = virtual_systems
 
         self
       end
--- spec/lib/palo-alto/models/device_spec.rb
+++ spec/lib/palo-alto/models/device_spec.rb
@@ -1,12 +1,14 @@
 require "palo-alto/models/device"
 
 describe "PaloAlto::Models::Device" do
-  let(:name) { "test-device" }
-  let(:ip)   { "2.2.2.2" }
+  let(:name)            { "test-device" }
+  let(:ip)              { "2.2.2.2" }
+  let(:virtual_systems) { [ "a", "b" ] }
 
   before do
-    @device = PaloAlto::Models::Device.new(name: name,
-                                           ip:   ip)
+    @device = PaloAlto::Models::Device.new(name:            name,
+                                           ip:              ip,
+                                           virtual_systems: virtual_systems)
   end
 
   it "has a name attribute" do
@@ -15,6 +17,10 @@ describe "PaloAlto::Models::Device" do
 
   it "has an ip attribute" do
     expect(@device).to respond_to(:ip)
+  end
+
+  it "has a virtual_systems attribute" do
+    expect(@device).to respond_to(:virtual_systems)
   end
 
   describe ".initialize" do
@@ -28,6 +34,10 @@ describe "PaloAlto::Models::Device" do
 
     it "assigns ip" do
       expect(@device.ip).to eq(ip)
+    end
+
+    it "assigns virtual_systems" do
+      expect(@device.virtual_systems).to eq(virtual_systems)
     end
   end
 end
