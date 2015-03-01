29069273c9fbefb8e66ae82a799d1b51d4ef0385
Justin Karimi <jekhokie@gmail.com>
2015-02-28 22:00:12 -0500

    Add .description to AddressGroup Model
    Add the .description attribute to the AddressGroup model.

--- lib/palo-alto/models/address-group.rb
+++ lib/palo-alto/models/address-group.rb
@@ -1,19 +1,22 @@
 module PaloAlto
   module Models
     class AddressGroup
-      attr_accessor :name
+      attr_accessor :name, :description
 
       # Create and returns a new PaloAlto::Models::AddressGroup instance with the given parameters
       #
       # == Attributes
       #
-      # * +name+ - Name of the address group
+      # * +name+        - Name of the address group
+      # * +description* - Description for the address group
       #
       # == Example
       #
-      #  PaloAlto::Models::AddressGroup.new name: 'address-group-1'
-      def initialize(name:)
-        self.name = name
+      #  PaloAlto::Models::AddressGroup.new name:        'address-group-1',
+      #                                     description: 'address-group-1-description'
+      def initialize(name:, description:)
+        self.name        = name
+        self.description = description
 
         self
       end
--- spec/lib/palo-alto/models/address_group_spec.rb
+++ spec/lib/palo-alto/models/address_group_spec.rb
@@ -1,14 +1,19 @@
 require "palo-alto/models/address-group"
 
 describe "PaloAlto::Models::AddressGroup" do
-  let(:name) { "test-address-group" }
+  let(:name)        { "test-address-group" }
+  let(:description) { "test-address-group-description" }
 
   before do
-    @address_group = PaloAlto::Models::AddressGroup.new(name: name)
+    @address_group = PaloAlto::Models::AddressGroup.new(name: name, description: description)
   end
 
   it "has a name attribute" do
     expect(@address_group).to respond_to(:name)
+  end
+
+  it "has a description attribute" do
+    expect(@address_group).to respond_to(:description)
   end
 
   describe ".initialize" do
@@ -18,6 +23,10 @@ describe "PaloAlto::Models::AddressGroup" do
 
     it "assigns name" do
       expect(@address_group.name).to eq(name)
+    end
+
+    it "assigns description" do
+      expect(@address_group.description).to eq(description)
     end
   end
 end
