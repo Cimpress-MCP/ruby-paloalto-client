49d27b58b31f8fbfe093bf2ae36c84b58744c113
Justin Karimi <jekhokie@gmail.com>
2015-03-01 01:28:06 -0500

    Spec and Add VirtualSystem Associations
    Update the VirtualSystem model to include associations with addresses,
    address groups and rulebases.

--- lib/palo-alto/models/virtual-system.rb
+++ lib/palo-alto/models/virtual-system.rb
@@ -1,19 +1,25 @@
 module PaloAlto
   module Models
     class VirtualSystem
-      attr_accessor :name
+      attr_accessor :name, :addresses, :address_groups, :rulebases
 
       # Create and returns a new PaloAlto::Models::VirtualSystem instance with the given parameters
       #
       # == Attributes
       #
-      # * +name+ - Name of the virtual system
+      # * +name+           - Name of the virtual system
+      # * +addresses+      - Array of Model::Address instances
+      # * +address_groups+ - Array of Model::AddressGroup instances
+      # * +rulebases+      - Array of Model::Rulebase instances
       #
       # == Example
       #
       #  PaloAlto::Models::VirtualSystem.new name: 'vsys-1'
-      def initialize(name:)
-        self.name = name
+      def initialize(name:, addresses: [], address_groups: [], rulebases: [])
+        self.name           = name
+        self.addresses      = addresses
+        self.address_groups = address_groups
+        self.rulebases      = rulebases
 
         self
       end
--- spec/lib/palo-alto/models/virtual_system_spec.rb
+++ spec/lib/palo-alto/models/virtual_system_spec.rb
@@ -1,14 +1,32 @@
 require "palo-alto/models/virtual-system"
 
 describe "PaloAlto::Models::VirtualSystem" do
-  let(:name) { "vsys-1" }
+  let(:name)           { "vsys-1" }
+  let(:addresses)      { [ "a", "b" ] }
+  let(:address_groups) { [ "c", "d" ] }
+  let(:rulebases)      { [ "e", "f" ] }
 
   before do
-    @virtual_system = PaloAlto::Models::VirtualSystem.new(name: name)
+    @virtual_system = PaloAlto::Models::VirtualSystem.new(name:           name,
+                                                          addresses:      addresses,
+                                                          address_groups: address_groups,
+                                                          rulebases:      rulebases)
   end
 
   it "has a name attribute" do
     expect(@virtual_system).to respond_to(:name)
+  end
+
+  it "has an addresses attribute" do
+    expect(@virtual_system).to respond_to(:addresses)
+  end
+
+  it "has an address_groups attribute" do
+    expect(@virtual_system).to respond_to(:address_groups)
+  end
+
+  it "has a rulebases attribute" do
+    expect(@virtual_system).to respond_to(:rulebases)
   end
 
   describe ".initialize" do
@@ -18,6 +36,18 @@ describe "PaloAlto::Models::VirtualSystem" do
 
     it "assigns name" do
       expect(@virtual_system.name).to eq(name)
+    end
+
+    it "assigns addresses" do
+      expect(@virtual_system.addresses).to eq(addresses)
+    end
+
+    it "assigns address_groups" do
+      expect(@virtual_system.address_groups).to eq(address_groups)
+    end
+
+    it "assigns rulebases" do
+      expect(@virtual_system.rulebases).to eq(rulebases)
     end
   end
 end
