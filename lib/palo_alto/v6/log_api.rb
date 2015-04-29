939960530d76323fb1ae36c7f44671071b96ad01
Justin Karimi <jekhokie@gmail.com>
2015-04-29 17:50:34 -0400

    Update Log API for Obtaining Logs
    Update the Log API and spec to allow capturing logs based on a Job ID.

--- lib/palo_alto/v6/log_api.rb
+++ lib/palo_alto/v6/log_api.rb
@@ -1,10 +1,8 @@
+require "palo_alto/models/log_entry"
+
 module PaloAlto
   module V6
     module LogApi
-      LOG_TYPES = [ "traffic", "threat",   "config",
-                    "system",  "hipmatch", "wildfire",
-                    "url",     "data" ]
-
       # Kicks off a job to generate logs asynchronously
       #
       # == Parameters
@@ -20,7 +18,7 @@ module PaloAlto
       #  * +Exception+ - Raises an exception if the request is unsuccessful or an
       #                  invalid log_type parameter is passed
       def generate_logs(log_type:)
-        raise "Invalid log_type - must be one of #{LOG_TYPES}" unless LOG_TYPES.include?(log_type)
+        raise "Invalid log_type - must be one of #{PaloAlto::Models::LogEntry::SUPPORTED_TYPES}" unless PaloAlto::Models::LogEntry::SUPPORTED_TYPES.include?(log_type)
 
         log_job_id = ''
 
@@ -47,6 +45,57 @@ module PaloAlto
         end
 
         log_job_id
+      end
+
+      # Gets a set of logs based on the Job ID
+      #
+      # == Parameters
+      #
+      #  * +job_id+ - ID of the job that generated the logs
+      #
+      # == Returns
+      #
+      #  * +Array+ - Array of Strings that are the log messages
+      #
+      # == Raises
+      #
+      #  * +Exception+ - Raises an exception if the request is unsuccessful
+      def get_logs(job_id:)
+        logs = []
+
+        # configure options for the request
+        options = {}
+        options[:url]     = self.endpoint
+        options[:method]  = :post
+        options[:payload] = { :type     => "log",
+                              :action   => :get,
+                              :'job-id' => job_id,
+                              :key      => self.auth_key }
+
+        html_result = Helpers::Rest.make_request(options)
+
+        raise "Error obtaining logs XML" if html_result.nil?
+
+        # parse the XML data
+        data          = Nokogiri::XML(html_result)
+        response_code = data.xpath('//response/@status').to_s
+
+        if response_code == "success"
+          # check if the job is finished
+          job_response_code = data.xpath('//response/result/job/status')[0].content.to_s
+
+          if job_response_code == "FIN"
+            data.xpath('//response/result/log/logs/*').each do |log_xml|
+              logs << PaloAlto::Models::LogEntry.from_xml(xml_data: log_xml)
+            end
+          else
+            raise "Log job with ID '#{job_id}' is still in progress"
+          end
+        else
+          raise "Error in response XML: #{data.inspect}"
+        end
+
+        logs
       end
     end
   end
--- spec/lib/palo-alto/v6/log_api_spec.rb
+++ spec/lib/palo-alto/v6/log_api_spec.rb
@@ -46,4 +46,53 @@ describe "PaloAlto::V6::LogApi" do
       end
     end
   end
+
+  describe ".get_logs" do
+    let(:job_id)          { "2014" }
+    let(:log_xml)         { File.open(fixture_file("traffic_logs.xml")).read }
+    let(:blank_log_xml)   { File.open(fixture_file("blank_traffic_logs.xml")).read }
+    let(:pending_log_xml) { File.open(fixture_file("pending_traffic_logs.xml")).read }
+
+    describe "when logs exist" do
+      it "parses the XML response into the required format" do
+        expect(PaloAlto::Helpers::Rest).to receive(:make_request).and_return(log_xml)
+        @logs = DummyClass.get_logs(job_id: job_id)
+
+        expect(@logs).to be_instance_of(Array)
+        expect(@logs).to_not be_empty
+      end
+    end
+
+    describe "when no logs exist" do
+      before do
+        expect(PaloAlto::Helpers::Rest).to receive(:make_request).and_return(blank_log_xml)
+      end
+
+      it "returns an empty array" do
+        expect(DummyClass.get_logs(job_id: job_id)).to eq([])
+      end
+    end
+
+    describe "when the log job is not complete" do
+      before do
+        expect(PaloAlto::Helpers::Rest).to receive(:make_request).and_return(pending_log_xml)
+      end
+
+      it "returns an Exception" do
+        expect{ DummyClass.get_logs(job_id: job_id) }.to raise_exception
+      end
+    end
+
+    describe "when errors occur" do
+      it "raises an exception if an error occurred obtaining XML" do
+        expect(PaloAlto::Helpers::Rest).to receive(:make_request).and_raise(Exception)
+        expect{ DummyClass.get_logs(job_id: job_id) }.to raise_exception
+      end
+
+      it "raises an exception if an error occurred reported in the XML" do
+        expect(PaloAlto::Helpers::Rest).to receive(:make_request).and_return(File.open(fixture_file("failure.xml")).read)
+        expect{ DummyClass.get_logs(job_id: job_id) }.to raise_exception
+      end
+    end
+  end
 end
